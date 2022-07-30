74c464c74ffd49ca007f678ebcda4476b282cad5
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-07-30 16:24:19 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <input v-model="inputValue" v-on:keyup.f2="onF2" />
+    <div class="np-credits">wwww.nightprogrammer.com</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    data() {
+      return {
+        inputValue: "",
+      };
+    },
+    onF2() {
+      alert("F2 triggerd!");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 24px;
+}
+
+.np-credits {
+  font-size: 12px;
+  margin-top: 12px;
+  color: #4b4b4b;
+}
+</style>
